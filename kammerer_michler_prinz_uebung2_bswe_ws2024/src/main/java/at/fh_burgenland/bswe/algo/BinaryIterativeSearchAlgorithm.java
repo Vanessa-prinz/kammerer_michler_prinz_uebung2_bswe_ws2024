931e66d1ca84a2cf4020b018f7199930c3448693
KammererLara <lara.kammerer@gmail.com>
2024-11-03 09:55:02 +0100

    erste Iteration BinarySearch

--- kammerer_michler_prinz_uebung2_bswe_ws2024/src/main/java/at/fh_burgenland/bswe/algo/BinaryIterativeSearchAlgorithm.java
+++ kammerer_michler_prinz_uebung2_bswe_ws2024/src/main/java/at/fh_burgenland/bswe/algo/BinaryIterativeSearchAlgorithm.java
@@ -1,10 +1,20 @@
 package at.fh_burgenland.bswe.algo;
 
+import java.util.Arrays;
+
 public class BinaryIterativeSearchAlgorithm extends SearchAlgorithm {
-    //TODO: -1 wenn number not found
 
     @Override
     public int execute(int[] list, int searchedNumber) {
-        return 0;
+        if (list == null
+                || list.length == 0
+                || (list.length == 1 && list[0] != searchedNumber))
+            return -1;
+        if (list[list.length/2] == searchedNumber)
+            return list.length/2;
+        else if (list[list.length/2] > searchedNumber)
+            return execute(Arrays.copyOfRange(list, 0, list.length/2), searchedNumber);
+        else
+            return execute(Arrays.copyOfRange(list, list.length/2, list.length), searchedNumber);
     }
 }
--- kammerer_michler_prinz_uebung2_bswe_ws2024/src/main/java/at/fh_burgenland/bswe/algo/BinaryRecursiveSearchAlgorithm.java
+++ kammerer_michler_prinz_uebung2_bswe_ws2024/src/main/java/at/fh_burgenland/bswe/algo/BinaryRecursiveSearchAlgorithm.java
@@ -1,10 +1,21 @@
 package at.fh_burgenland.bswe.algo;
 
+import java.util.Arrays;
+
 public class BinaryRecursiveSearchAlgorithm extends SearchAlgorithm {
-    //TODO: -1 wenn number not found
 
     @Override
     public int execute(int[] list, int searchedInteger) {
-        return 0;
+        while (true) {
+            if (list[list.length/2] == searchedInteger)
+                return list.length/2;
+            else if (list[list.length/2] < searchedInteger)
+                list = Arrays.copyOfRange(list, list.length/2, list.length);
+            else
+                list  = Arrays.copyOfRange(list, 0, list.length/2);
+
+            if (list.length == 1 && list[0] != searchedInteger)
+                return -1;
+        }
     }
 }
